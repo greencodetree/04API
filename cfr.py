718f71d1dec0803fc02c393ca3bd01e296d7af67
greencodetree <58033423+greencodetree@users.noreply.github.com>
2020-01-07 00:05:24 +0900

    Added cfr.py
    In progress. Uses API from naver (Clova Face Recognition) for facial recognition from an image(BidenObama.jpg).

---
+++ cfr.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import requests
+
+client_id = "v90srga7toF_IwP3E_QL"
+client_secret = "1ExAb21HCk"
+
+url = "https://openapi.naver.com/v1/vision/face" # 얼굴감지
+#url = "https://openapi.naver.com/v1/vision/celebrity" # 유명인 얼굴인식
+
+files = {'image': open('BidenObama.jpg', 'rb')}
+headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
+response = requests.post(url,  files=files, headers=headers)
+rescode = response.status_code
+
+if(rescode==200):
+    print (response.text)
+else:
+    print("Error Code:" + rescode)
