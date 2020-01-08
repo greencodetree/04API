5d4edb968f7427e39d6589ee1e90e66c9f4f9d85
greencodetree <58033423+greencodetree@users.noreply.github.com>
2020-01-08 23:47:53 +0900

    Finished cfr.py, papago.py
    cfr.py uses clova facial recognition api and outputs a JSON data that analyzes and identifies a celebrity's face. papago.py translates korean text in source.txt into english, and creates and saves it in translate.txt. When any changes are made eg.change in translation language/text, changes will be overwritten in translate.txt.

--- cfr.py
+++ cfr.py
@@ -1,12 +1,14 @@
 import os
 import sys
 import requests
+from pprint import pprint
+import json
 
 client_id = "v90srga7toF_IwP3E_QL"
 client_secret = "1ExAb21HCk"
 
-url = "https://openapi.naver.com/v1/vision/face" # 얼굴감지
-#url = "https://openapi.naver.com/v1/vision/celebrity" # 유명인 얼굴인식
+#url = "https://openapi.naver.com/v1/vision/face" # 얼굴감지
+url = "https://openapi.naver.com/v1/vision/celebrity" # 유명인 얼굴인식
 
 files = {'image': open('BidenObama.jpg', 'rb')}
 headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
@@ -14,6 +16,10 @@ response = requests.post(url,  files=files, headers=headers)
 rescode = response.status_code
 
 if(rescode==200):
-    print (response.text)
+    #print (response.text)
+    data = json.loads(response.text)
+    pprint(data)
+    print(type(data))
+    print(data['info']['faceCount'])
 else:
     print("Error Code:" + rescode)
---
+++ papago.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import urllib.request
+import json
+from pprint import pprint
+
+#메모장 불러오기
+with open('source.txt','r',encoding='utf8') as f:
+    srcText = f.read()
+
+client_id = "v90srga7toF_IwP3E_QL" # 개발자센터에서 발급받은 Client ID 값
+client_secret = "1ExAb21HCk" # 개발자센터에서 발급받은 Client Secret 값
+
+encText = urllib.parse.quote(srcText)  #번역할 내용
+data = "source=ko&target=en&text=" + encText    #원본 언어와 바꿀 언어
+url = "https://openapi.naver.com/v1/papago/n2mt"
+
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+
+response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+rescode = response.getcode()
+
+if(rescode==200):
+    response_body = response.read()
+    #print(response_body.decode('utf-8'))
+    res = json.loads(response_body.decode('utf-8'))
+    pprint(res)
+
+    #파일 생성
+    with open('translate.txt', 'w', encoding='utf8') as f:
+        f.write(res['message']['result']['translatedText'])
+else:
+    print("Error Code:" + rescode)
